c5b6b85419f16332c604e5adfd66e6bc4fec0c9a
Suellen Santos <isuelima@gmail.com>
2026-02-04 19:56:55 -0300

    App funcionando

--- frontend/src/config.py
+++ frontend/src/config.py
@@ -1,5 +1,5 @@
 import os
 
-API_URL = os.getenv("API_URL", "http://backend:8000")
+API_URL = os.getenv("API_URL", "http://localhost:8000")
 APP_TITLE = os.getenv("APP_TITLE", "Energy Monitor Pro")
 LAYOUT = os.getenv("APP_LAYOUT", "wide")
