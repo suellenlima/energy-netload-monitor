679dd30771ccd119160d3dabf43b0d04b3c65000
Suellen Santos <isuelima@gmail.com>
2026-02-05 19:14:12 -0300

    Fix Subsistemas, Refactor ETL aneel local

--- backend/src/api/health.py
+++ backend/src/api/health.py
@@ -8,7 +8,7 @@ router = APIRouter()
 
 @router.get("/")
 def read_root():
-    return {"message": "Energy Monitor Online ⚡"}
+    return {"message": "EFICIENERGY Online ⚡"}
 
 
 @router.get("/health")
